7d5e279a949ad5cb4e4be9481aab714d4364c53b
Naima <bilalnaima1911@gmail.com>
2022-09-11 23:01:44 +0300

    создаю методы Input и Print для класса Point

--- dz4444/dz4444.cpp
+++ dz4444/dz4444.cpp
@@ -14,6 +14,22 @@ public:
 		z = 0;
 	}
 
+	void Input()
+	{
+		cout << "Input X = ";
+		cin >> x;
+		cout << "Input Y = ";
+		cin >> y;
+		cout << "Input Z = ";
+		cin >> z;
+	}
+
+	void Print()
+	{
+		cout << "X = " << x;
+		cout << "\nY = " << y;
+		cout << "\nZ = " << z;
+	}
 };
 int main()
 {
